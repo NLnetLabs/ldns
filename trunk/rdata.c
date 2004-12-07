abfbae9e5274a3979f7cafcdc3759ee17905a13c
Miek Gieben <miekg@NLnetLabs.nl>
2004-12-07 11:16:59 +0000

    splint checks

--- trunk/rdata.c
+++ trunk/rdata.c
@@ -11,6 +11,7 @@
  */
 
 #include <stdint.h>
+#include <string.h>
 
 #include "prototype.h"
 
@@ -51,9 +52,10 @@ rd_set_type(rdata_t *rd, rd_type_t t)
 }
 
 void
-rd_set_data(rdata_t *rd, uint8_t *d)
+rd_set_data(rdata_t *rd, uint8_t *d, uint16_t s)
 {
-	rd->_data = d;
+	rd->_data = xmalloc(s);
+	memcpy(rd->_data, d, s);
 }
 
 /* allocate a new rdata_t structure 
@@ -70,7 +72,29 @@ rd_new(uint16_t s, rd_type_t t, uint8_t *d)
 
 	rd_set_size(new, s);
 	rd_set_type(new, t);
-	rd_set_data(new, d);
+	rd_set_data(new, d, s);
+
+	return(new);
+}
+
+/* allocate a new rdata_t from
+ * a NULL terminated string
+ * and return it
+ *
+ * uint8_t == char !!!!
+ */
+rdata_t *
+rd_new_frm_string(rd_type_t t, char *s)
+{
+	rdata_t *new;
+	new = xmalloc(sizeof(rdata_t));
+
+	if (NULL == new)
+		return NULL;
+
+	rd_set_size(new, (uint16_t)strlen(s));
+	rd_set_type(new, t);
+	rd_set_data(new, (uint8_t*) s, (uint16_t)strlen(s));
 
 	return(new);
 }
