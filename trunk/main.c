a41af0a3b2f8f2d5e12b7086468c28704ba07ad8
Miek Gieben <miekg@NLnetLabs.nl>
2004-12-09 14:21:03 +0000

    more stuff implemented

--- trunk/main.c
+++ trunk/main.c
@@ -13,9 +13,13 @@
 int
 main(void)
 {
-	t_rdata_field *new;
+	t_rdata_field *rd_f;
+	t_rr *rr;
 
-	new = rd_field_new(20, RD_DNAME_T, (uint8_t*)"hallo.nl");
-	xprintf_rd_field(new);
+	rr = rr_new();
+	rd_f = rd_field_new(20, RD_DNAME_T, (uint8_t*)"hallo.nl");
+	xprintf_rd_field(rd_f);
+	
+	rr_push_rd_field(rr, rd_f);
 	return 0;
 }
