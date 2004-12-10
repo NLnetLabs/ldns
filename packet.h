2a372e1f438a1924d24ee8b2e3cf205b7be2cf23
Jelte Jansen <jeltejan@NLnetLabs.nl>
2004-12-10 14:25:17 +0000

    packet access functions started with dns_wire2packet fixed typos in configure.ac and packet.h

--- packet.h
+++ packet.h
@@ -12,9 +12,7 @@
 #ifdef _PACKET_H
 #else
 #define _PACKET_H
-
-#include "rdata.h"
-#include "rr.h"
+#include "config.h"
 
 /**
  * \brief Header of a dns packet
@@ -50,7 +48,7 @@ struct type_struct_header
 	/** \brief auth sec */
 	uint16_t nscount;
 	/** \brief add sec */
-	uint16_t acount;
+	uint16_t arcount;
 };
 typedef struct type_struct_header t_header;
 
@@ -59,7 +57,7 @@ typedef struct type_struct_header t_header;
  *
  * This structure contains a complete DNS packet (either a query or an answer)
  */
-struct struct_packet_type
+struct type_struct_packet
 {
 	/** \brief header section */
 	t_header *header;
@@ -73,5 +71,48 @@ struct struct_packet_type
 	t_rrset	*additional;
 };
 typedef struct type_struct_packet t_packet;
-	
-#endif /* _PACKET_H */
+
+/* prototypes */
+uint16_t packet_id(t_packet *);
+uint8_t packet_qr(t_packet *);
+uint8_t packet_aa(t_packet *);
+uint8_t packet_tc(t_packet *);
+uint8_t packet_rd(t_packet *);
+uint8_t packet_cd(t_packet *);
+uint8_t packet_ra(t_packet *);
+uint8_t packet_ad(t_packet *);
+uint8_t packet_opcode(t_packet *);
+uint8_t packet_rcode(t_packet *);
+uint16_t packet_qdcount(t_packet *);
+uint16_t packet_ancount(t_packet *);
+uint16_t packet_nscount(t_packet *);
+uint16_t packet_arcount(t_packet *);
+
+void packet_set_id(t_packet *, uint16_t);
+void packet_set_qr(t_packet *, uint8_t);
+void packet_set_aa(t_packet *, uint8_t);
+void packet_set_tc(t_packet *, uint8_t);
+void packet_set_rd(t_packet *, uint8_t);
+void packet_set_cd(t_packet *, uint8_t);
+void packet_set_ra(t_packet *, uint8_t);
+void packet_set_ad(t_packet *, uint8_t);
+void packet_set_opcode(t_packet *, uint8_t);
+void packet_set_rcode(t_packet *, uint8_t);
+void packet_set_qdcount(t_packet *, uint16_t);
+void packet_set_ancount(t_packet *, uint16_t);
+void packet_set_nscount(t_packet *, uint16_t);
+void packet_set_arcount(t_packet *, uint16_t);
+
+/**
+ * Allocates and initializes a t_packet structure
+ */
+t_packet *dns_packet_new();
+
+/**
+ * Converts the data on the uint8_t bytearray (in wire format) to a DNS packet
+ *
+ * Returns the number of bytes read from the wire
+ */
+size_t dns_wire2packet(uint8_t *, t_packet *);
+
+#endif
