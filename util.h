275dff4d7a606850fd7318bba8c93b10323a3544
Miek Gieben <miekg@NLnetLabs.nl>
2004-12-13 12:39:17 +0000

    ook nodig

---
+++ util.h
@@ -0,0 +1,41 @@
+/*
+ * util.h
+ *  
+ * helper function header file
+ * 
+ * a Net::DNS like library for C
+ * 
+ * (c) NLnet Labs, 2004
+ * 
+ * See the file LICENSE for the license
+ */
+
+#ifdef _UTIL_H
+#else
+#define _UTIL_H
+
+/**
+ * Memory management macro's
+ */
+#define MALLOC(ptr, type) \
+        XMALLOC((ptr), (type), 1)
+
+#define XMALLOC(ptr, type, count) \
+        (ptr) = ((type) *) malloc((count) * sizeof(type))
+
+#define REALLOC(ptr, type) \
+        XREALLOC((ptr), (type), 1)
+
+#define XREALLOC(ptr, type, count) \
+        (ptr) == ((type) *) realloc((count) * sizeof(type))
+
+#define FREE(ptr) \
+    do { free(ptr); (ptr) = NULL; } while (0)
+
+#define DEP     printf("DEPRICATED FUNCTION!\n");
+
+/* prototypes */
+void    xprintf_rd_field(t_rdata_field *);
+void    xprintf_rr(t_rr *);
+
+#endif /* _UTIL_H */
