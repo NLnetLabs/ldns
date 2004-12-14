9d5f0f2a7d16dfcd9ec925ab98660497451265e8
Jelte Jansen <jeltejan@NLnetLabs.nl>
2004-12-14 13:15:20 +0000

    field names

--- trunk/packet.c
+++ trunk/packet.c
@@ -26,85 +26,85 @@
 uint16_t
 packet_id(t_packet *packet)
 {
-	return packet->header->id;
+	return packet->_header->_id;
 }
 
 uint8_t
 packet_qr(t_packet *packet)
 {
-	return packet->header->qr;
+	return packet->_header->_qr;
 }
 
 uint8_t
 packet_aa(t_packet *packet)
 {
-	return packet->header->aa;
+	return packet->_header->_aa;
 }
 
 uint8_t
 packet_tc(t_packet *packet)
 {
-	return packet->header->tc;
+	return packet->_header->_tc;
 }
 
 uint8_t
 packet_rd(t_packet *packet)
 {
-	return packet->header->rd;
+	return packet->_header->_rd;
 }
 
 uint8_t
 packet_cd(t_packet *packet)
 {
-	return packet->header->cd;
+	return packet->_header->_cd;
 }
 
 uint8_t
 packet_ra(t_packet *packet)
 {
-	return packet->header->ra;
+	return packet->_header->_ra;
 }
 
 uint8_t
 packet_ad(t_packet *packet)
 {
-	return packet->header->ad;
+	return packet->_header->_ad;
 }
 
 uint8_t
 packet_opcode(t_packet *packet)
 {
-	return packet->header->opcode;
+	return packet->_header->_opcode;
 }
 
 uint8_t
 packet_rcode(t_packet *packet)
 {
-	return packet->header->rcode;
+	return packet->_header->_rcode;
 }
 
 uint16_t
 packet_qdcount(t_packet *packet)
 {
-	return packet->header->qdcount;
+	return packet->_header->_qdcount;
 }
 
 uint16_t
 packet_ancount(t_packet *packet)
 {
-	return packet->header->ancount;
+	return packet->_header->_ancount;
 }
 
 uint16_t
 packet_nscount(t_packet *packet)
 {
-	return packet->header->nscount;
+	return packet->_header->_nscount;
 }
 
 uint16_t
 packet_arcount(t_packet *packet)
 {
-	return packet->header->arcount;
+	return packet->_header->_arcount;
 }
 
 
@@ -112,85 +112,85 @@ packet_arcount(t_packet *packet)
 void
 packet_set_id(t_packet *packet, uint16_t id)
 {
-	packet->header->id = id;
+	packet->_header->_id = id;
 }
 
 void
 packet_set_qr(t_packet *packet, uint8_t qr)
 {
-	packet->header->qr = qr;
+	packet->_header->_qr = qr;
 }
 
 void
 packet_set_aa(t_packet *packet, uint8_t aa)
 {
-	packet->header->aa = aa;
+	packet->_header->_aa = aa;
 }
 
 void
 packet_set_tc(t_packet *packet, uint8_t tc)
 {
-	packet->header->tc = tc;
+	packet->_header->_tc = tc;
 }
 
 void
 packet_set_rd(t_packet *packet, uint8_t rd)
 {
-	packet->header->rd = rd;
+	packet->_header->_rd = rd;
 }
 
 void
 packet_set_cd(t_packet *packet, uint8_t cd)
 {
-	packet->header->cd = cd;
+	packet->_header->_cd = cd;
 }
 
 void
 packet_set_ra(t_packet *packet, uint8_t ra)
 {
-	packet->header->ra = ra;
+	packet->_header->_ra = ra;
 }
 
 void
 packet_set_ad(t_packet *packet, uint8_t ad)
 {
-	packet->header->ad = ad;
+	packet->_header->_ad = ad;
 }
 
 void
 packet_set_opcode(t_packet *packet, uint8_t opcode)
 {
-	packet->header->opcode = opcode;
+	packet->_header->_opcode = opcode;
 }
 
 void
 packet_set_rcode(t_packet *packet, uint8_t rcode)
 {
-	packet->header->rcode = rcode;
+	packet->_header->_rcode = rcode;
 }
 
 void
 packet_set_qdcount(t_packet *packet, uint16_t qdcount)
 {
-	packet->header->qdcount = qdcount;
+	packet->_header->_qdcount = qdcount;
 }
 
 void
 packet_set_ancount(t_packet *packet, uint16_t ancount)
 {
-	packet->header->ancount = ancount;
+	packet->_header->_ancount = ancount;
 }
 
 void
 packet_set_nscount(t_packet *packet, uint16_t nscount)
 {
-	packet->header->nscount = nscount;
+	packet->_header->_nscount = nscount;
 }
 
 void
 packet_set_arcount(t_packet *packet, uint16_t arcount)
 {
-	packet->header->arcount = arcount;
+	packet->_header->_arcount = arcount;
 }
 
 
@@ -206,15 +206,15 @@ dns_packet_new()
 		return NULL;
 	}
 
-	MALLOC(packet->header, t_header);
-	if (!packet->header) {
+	MALLOC(packet->_header, t_header);
+	if (!packet->_header) {
 		return NULL;
 	}
 
-	packet->question = NULL;
-	packet->answer = NULL;
-	packet->authority = NULL;
-	packet->additional = NULL;
+	packet->_question = NULL;
+	packet->_answer = NULL;
+	packet->_authority = NULL;
+	packet->_additional = NULL;
 	return packet;
 }
 
