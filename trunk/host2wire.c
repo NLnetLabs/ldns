20e2046ab2e863c68f0c7782322bcd83db32e483
Miek Gieben <miekg@NLnetLabs.nl>
2004-12-16 11:55:10 +0000

    conversion file placeholders

---
+++ trunk/host2wire.c
@@ -0,0 +1,16 @@
+/*
+ * wire2host.c
+ *
+ * conversion routines from the host to the wire format.
+ * This will usually just a re-ordering of the
+ * data (as we store it in network format)
+ *
+ * XXX not sure if we need to keep this around
+ *
+ * a Net::DNS like library for C
+ *
+ * (c) NLnet Labs, 2004
+ *
+ * See the file LICENSE for the license
+ */
+
