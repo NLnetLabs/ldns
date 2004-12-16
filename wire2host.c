0f33d8ffba2e86794728d0d78111b3a385b95209
Miek Gieben <miekg@NLnetLabs.nl>
2004-12-16 11:55:10 +0000

    conversion file placeholders

---
+++ wire2host.c
@@ -0,0 +1,15 @@
+/*
+ * wire2host.c
+ *
+ * conversion routines from the wire to the host
+ * format.
+ * This will usually just a re-ordering of the
+ * data (as we store it in network format)
+ *
+ * a Net::DNS like library for C
+ *
+ * (c) NLnet Labs, 2004
+ *
+ * See the file LICENSE for the license
+ */
+
