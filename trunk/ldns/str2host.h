2f2e5cfd5a1051185b226b5c81059bf35d5cb931
Miek Gieben <miekg@NLnetLabs.nl>
2004-12-21 15:01:36 +0000

    still leave out the htons

--- trunk/ldns/str2host.h
+++ trunk/ldns/str2host.h
@@ -1,4 +1,3 @@
-#include <ldns/rdata.h>
 
 #ifndef _LDNS_STR2HOST_H
 #define _LDNS_STR2HOST_H
@@ -6,6 +5,7 @@
 #include <ldns/common.h>
 #include <ldns/error.h>
 #include <ldns/rr.h>
+#include <ldns/rdata.h>
 #include <ldns/packet.h>
 #include <ldns/buffer.h>
 #include <ctype.h>
