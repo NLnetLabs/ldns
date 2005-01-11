ae083e6bb850eae1082cf253308cd9b896aa2396
Miek Gieben <miekg@NLnetLabs.nl>
2005-01-11 12:44:26 +0000

    net.c: put all your net stuff here

---
+++ trunk/net.c
@@ -0,0 +1,20 @@
+/*
+ * net.c
+ *
+ * Network implementation
+ * All network related functions are grouped here
+ *
+ * a Net::DNS like library for C
+ *
+ * (c) NLnet Labs, 2004
+ *
+ * See the file LICENSE for the license
+ */
+
+#include <config.h>
+
+#include <ldns/rdata.h>
+#include <ldns/error.h>
+#include <ldns/resolver.h>
+
+#include "util.h"
