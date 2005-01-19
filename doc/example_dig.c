c5b2e873694e3f65cfb22fbf0dc5345044340d27
Miek Gieben <miekg@NLnetLabs.nl>
2005-01-19 14:06:22 +0000

    jelte comments

--- doc/example_dig.c
+++ doc/example_dig.c
@@ -12,32 +12,34 @@
 int 
 main(void)
 {
-	ldns_resolver *Res;
+	ldns_resolver *res;
 	ldns_rdf *default_dom;
+	ldns_rdf *nameserver;
 	ldns_rdf *qname;
-	ldns_rr_type *qtype;
 	ldns_pkt *pkt;
 
 	/* init */
-	Res = ldns_resolver_new();
-	if (!Res)
+	res = ldns_resolver_new();
+	if (!res)
 		return 1;
 	
 	/* create a default domain and add it */
-	default_dom = rdf_new_frm_str("miek.nl", LDNS_RDF_TYPE_DNAME);
-	if (ldns_resolver_nameserver_push(Res, default_dom) !=
-			LDNS_STATUS_OK)
+	default_dom = ldns_rdf_new_frm_str("miek.nl.", LDNS_RDF_TYPE_DNAME);
+	nameserver  = ldns_rdf_new_from_str("127.0.0.1", LDNS_RDF_TYPE_A);
+
+	if (ldns_resolver_domain(res, default_dom) != LDNS_STATUS_OK)
+		return 1;
+	if (ldns_resolver_nameserver_push(res, nameserver) != LDNS_STATUS_OK)
 		return 1;
 
 	/* setup the question */
 	qname = ldns_rdf_new_frm_str("www", LDNS_RDF_TYPE_DNAME);
-	qtype = ldns_rr_type_new_frm_str("MX");
 
-	/* fire it off */
-	pkt = ldns_resolver_query(Res, qname, qtype, NULL);
+	/* fire it off. "miek.nl." will be added */
+	pkt = ldns_resolver_query(res, qname, LDNS_RR_TYPE_MX, NULL);
 
 	/* print the resulting pkt to stdout */
-	ldns_pkt_print(Res, stdout);
+	ldns_pkt_print(stdout, res);
 
 	return 0;
 }
