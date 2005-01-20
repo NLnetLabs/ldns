019ff60ca21c4d0265d0b6a17700a08693d0c9c9
Miek Gieben <miekg@NLnetLabs.nl>
2005-01-20 13:53:38 +0000

    function names more inline with the rest

--- trunk/ldns/str2host.h
+++ trunk/ldns/str2host.h
@@ -10,28 +10,27 @@
 #include <ldns/buffer.h>
 #include <ctype.h>
 
-ldns_status ldns_conv_int8(ldns_rdf **, const uint8_t *);
-ldns_status ldns_conv_int16(ldns_rdf **, const uint8_t *);
-ldns_status ldns_conv_int32(ldns_rdf **, const uint8_t *);
-ldns_status ldns_conv_time(ldns_rdf **, const uint8_t *);
-ldns_status ldns_conv_none(ldns_rdf **, const uint8_t* );
-ldns_status ldns_conv_dname(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_a(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_aaaa(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_str(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_apl(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_b64(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_hex(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_nsec(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_type(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_class(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_cert(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_alg(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_unknown(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_tsigtime(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_service(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_loc(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_wks(ldns_rdf **, const uint8_t*);
-ldns_status ldns_conv_nsap(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_int8(ldns_rdf **, const uint8_t *);
+ldns_status ldns_str2rdf_int16(ldns_rdf **, const uint8_t *);
+ldns_status ldns_str2rdf_int32(ldns_rdf **, const uint8_t *);
+ldns_status ldns_str2rdf_time(ldns_rdf **, const uint8_t *);
+ldns_status ldns_str2rdf_dname(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_a(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_aaaa(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_str(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_apl(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_b64(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_hex(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_nsec(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_type(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_class(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_cert(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_alg(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_unknown(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_tsigtime(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_service(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_loc(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_wks(ldns_rdf **, const uint8_t*);
+ldns_status ldns_str2rdf_nsap(ldns_rdf **, const uint8_t*);
 
 #endif
