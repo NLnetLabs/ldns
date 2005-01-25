d9313aad3a6b6e9d3ea95422cfee060b70652cff
Miek Gieben <miekg@NLnetLabs.nl>
2005-01-25 14:06:16 +0000

    HUGE cleanup of the resolver

--- trunk/ldns/resolver.h
+++ trunk/ldns/resolver.h
@@ -29,8 +29,9 @@ struct ldns_struct_resolver
 	/** \brief On which port to run */
 	uint16_t _port;
 
-	/** \brief List of nameservers to query */
-	ldns_rr_list *_nameservers; 
+	/** \brief List of nameservers to query (IP addresses or dname) */
+	ldns_rdf **_nameservers; 
+	uint8_t _nameserver_count; /* how many do we have */
 
 	/** \brief Wether or not to be recursive */
 	uint8_t _recursive;
@@ -38,12 +39,12 @@ struct ldns_struct_resolver
 	/** \brief Print debug information */
 	uint8_t _debug;
 	
-	/* XXX both types below could be done better, mabye rr_list? */
 	/** \brief Default domain to add */
-	ldns_rdf *_domain; /* LDNS_RDF_TYPE_DNAME */
+	ldns_dname *_domain; /* LDNS_RDF_TYPE_DNAME */
 
-	/** \brief Searchlist */
-	ldns_rdf *_searchlist; /* LDNS_RFD_TYPE_DNAME */
+	/** \brief Searchlist array */
+	ldns_dname **_searchlist; /* LDNS_RFD_TYPE_DNAME */
+	uint8_t _searchlist_count;
 
 	/** \brief How many retries */
 	uint8_t _retry;
@@ -64,22 +65,26 @@ typedef struct ldns_struct_resolver ldns_resolver;
 
 /* prototypes */
 uint16_t ldns_resolver_port(ldns_resolver *);
-ldns_rr_list * ldns_resolver_nameservers(ldns_resolver *);
+/* ldns_rr_list * ldns_resolver_nameservers(ldns_resolver *) pop>? */
 uint8_t ldns_resolver_recursive(ldns_resolver *);
 uint8_t ldns_resolver_debug(ldns_resolver *);
-ldns_rdf * ldns_resolver_domain(ldns_resolver *);
-ldns_rdf * ldns_resolver_searchlist(ldns_resolver *);
+ldns_dname * ldns_resolver_domain(ldns_resolver *);
+ldns_dname ** ldns_resolver_searchlist(ldns_resolver *);
+ldns_dname ** ldns_resolver_nameservers(ldns_resolver *);
 
 void ldns_resolver_set_port(ldns_resolver *, uint16_t);
-void ldns_resolver_set_nameservers(ldns_resolver *, ldns_rr_list *);
 void ldns_resolver_set_recursive(ldns_resolver *, uint8_t);
 void ldns_resolver_set_debug(ldns_resolver *, uint8_t);
-void ldns_resolver_set_domain(ldns_resolver *, ldns_rdf *);
-void ldns_resolver_set_searchlist(ldns_resolver *, ldns_rdf *);
+void ldns_resolver_set_domain(ldns_resolver *, ldns_dname *);
+
+ldns_status ldns_resolver_push_searchlist(ldns_resolver *, ldns_dname *);
+ldns_status ldns_resolver_push_nameserver(ldns_resolver *, ldns_rdf *);
 
 ldns_pkt * ldns_search();
 ldns_pkt * ldns_query();
 ldns_pkt * ldns_bgsend();
 ldns_pkt * ldns_send(ldns_resolver *, ldns_dname*, ldns_rr_type *, ldns_rr_class*);
+
+ldns_resolver ldns_resolver_new(void);
 
 #endif  /* !_LDNS_RESOLVER_H */
