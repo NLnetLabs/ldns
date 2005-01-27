e0b648a6c9c218d4cdae3133bdaf24930b4c026a
Jelte Jansen <jeltejan@NLnetLabs.nl>
2005-01-27 12:52:19 +0000

    oops

---
+++ ldns/host2wire.h
@@ -0,0 +1,19 @@
+
+#ifndef _LDNS_HOST2WIRE_H
+#define _LDNS_HOST2WIRE_H
+
+#include <ldns/common.h>
+#include <ldns/error.h>
+#include <ldns/rr.h>
+#include <ldns/rdata.h>
+#include <ldns/packet.h>
+#include <ldns/buffer.h>
+#include <ctype.h>
+
+#include "util.h"
+
+ldns_status ldns_rdf2wire(ldns_buffer *buffer, ldns_rdf *rdf);
+ldns_status ldns_rr2wire(ldns_buffer *buffer, ldns_rr *rr, int section);
+ldns_status ldns_pkt2wire(ldns_buffer *buffer, ldns_pkt *pkt);
+
+#endif
