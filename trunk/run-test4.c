0b246e1146e06a287b857d9419224e3a00be792f
Miek Gieben <miekg@NLnetLabs.nl>
2005-02-01 13:12:44 +0000

    debugging debugging

--- trunk/run-test4.c
+++ trunk/run-test4.c
@@ -29,7 +29,7 @@ main(void)
 		printf("error default dom\n");
 		return 1;
 	}
-        nameserver  = ldns_rdf_new_frm_str("127.0.0.1", LDNS_RDF_TYPE_A);
+        nameserver  = ldns_rdf_new_frm_str("192.168.1.1", LDNS_RDF_TYPE_A);
 	if (!nameserver) {
 		printf("error default dom\n");
 		return 1;
