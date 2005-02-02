a5d39e25b2799d07deda1f52c21faf246be7fb3b
Miek Gieben <miekg@NLnetLabs.nl>
2005-02-02 12:17:12 +0000

    added printing for the extra flags. querytime is not set correctly

--- host2str.c
+++ host2str.c
@@ -825,7 +825,9 @@ ldns_pkt2buffer_str(ldns_buffer *output, ldns_pkt *pkt)
 			ldns_buffer_printf(output, "\n");
 		}
 		ldns_buffer_printf(output, "\n");
-		
+		/* add some futher fields */
+		ldns_buffer_printf(output, ";; Query time: %d msec\n", ldns_pkt_querytime(pkt));
+		ldns_buffer_printf(output, ";; SERVER: %s\n", ldns_pkt_answerfrom(pkt));
 	} else {
 		return ldns_buffer_status(output);
 	}
