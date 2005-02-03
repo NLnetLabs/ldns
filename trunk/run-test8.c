2029c847c0f70150438f9e3a82bc2cb61953f0d2
Miek Gieben <miekg@NLnetLabs.nl>
2005-02-03 11:34:52 +0000

    another runtest

---
+++ trunk/run-test8.c
@@ -0,0 +1,71 @@
+/**
+ * An example ldns program
+ *
+ * Setup a resolver
+ * Query a nameserver
+ * Print the result
+ */
+
+#include <config.h>
+#include <ldns/resolver.h>
+        
+
+void
+print_usage(char *file)
+{
+	printf("Usage: %s <type> <name> <server ip>\n", file);
+	printf("ipv4 only atm\n");
+	exit(0);
+}
+
+void
+main(int argc, char **argv)
+{       
+        ldns_resolver *res;
+        ldns_rdf *qname;
+        ldns_rdf *nameserver;
+        ldns_pkt *pkt;
+        char *server_ip;
+        char *name;
+        char *type;
+        
+        if (argc < 4) {
+        	print_usage(argv[0]);
+	} else {
+		type = argv[1];
+		name = argv[2];
+		server_ip = argv[3];
+	}
+                
+        /* init */
+        res = ldns_resolver_new(); 
+        if (!res)
+                return;
+
+        nameserver  = ldns_rdf_new_frm_str(server_ip, LDNS_RDF_TYPE_A);
+	if (!nameserver) {
+		printf("Bad server ip\n");
+		return;
+	}
+
+        if (ldns_resolver_push_nameserver(res, nameserver) != LDNS_STATUS_OK) {
+		printf("error push nameserver\n");
+		return;
+	}
+	/* HACK */
+	ldns_resolver_set_configured(res, 1);
+	/* TCP query */
+	ldns_resolver_set_usevc(res, true);
+        qname = ldns_rdf_new_frm_str(name, LDNS_RDF_TYPE_DNAME);
+	if (!qname) {
+		printf("error making qname\n");
+		return;
+	}
+        
+        pkt = ldns_resolver_send(res, qname, ldns_rr_get_type_by_name(type), 0, LDNS_RD);
+	if (!pkt)  {
+		printf("error pkt sending\n");
+		return;
+	}
+        ldns_pkt_print(stdout, pkt);
+}
