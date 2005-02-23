8f2d0d54cb860d37f32da8d6bd28dbb142c29131
Jelte Jansen <jeltejan@NLnetLabs.nl>
2005-02-23 13:54:30 +0000

    Split up the tcp functionality in net.c added run-test11, which is an example for an axfr transfer implementation test with ./run-test11 jelte.nlnetlabs.nl 195.169.215.155

--- trunk/ldns/net.h
+++ trunk/ldns/net.h
@@ -28,4 +28,8 @@ ldns_pkt * ldns_send_udp(ldns_buffer *, const struct sockaddr_storage *, socklen
 ldns_pkt * ldns_send_tcp(ldns_buffer *, const struct sockaddr_storage *, socklen_t);
 ldns_pkt * ldns_send(ldns_resolver *, ldns_pkt *);
 
+int ldns_tcp_connect(const struct sockaddr_storage *to, socklen_t tolen);
+ssize_t ldns_tcp_send_query(ldns_buffer *qbin, int sockfd, const struct sockaddr_storage *to, socklen_t tolen);
+ldns_pkt *ldns_tcp_read_packet(int sockfd);
+
 #endif  /* !_LDNS_NET_H */
