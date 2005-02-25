51df18838b3101983177f3d2bcbc57373c81bd9b
Miek Gieben <miekg@NLnetLabs.nl>
2005-02-25 14:01:14 +0000

    duh

---
+++ trunk/ldns/dnssec.h
@@ -0,0 +1,23 @@
+/*
+ * dnssec.h -- defines for the Domain Name System (SEC) (DNSSEC)
+ *
+ * Copyright (c) 2001-2005, NLnet Labs. All rights reserved.
+ *
+ * See LICENSE for the license.
+ *
+ * A bunch of defines that are used in the DNS
+ */
+
+#ifndef _DNSSEC_H_
+#define _DNSSEC_H_
+
+#define LDNS_RSAMD5		1
+#define LDNS_DH			2
+#define LDNS_DSA		3
+#define LDNS_ECC		4
+#define LDNS_RSASHA1		5
+#define LDNS_INDIRECT		252
+#define LDNS_PRIVATEDNS		253
+#define LDNS_PRIVATEOID		254
+
+#endif /* _DNSSEC_H_ */
