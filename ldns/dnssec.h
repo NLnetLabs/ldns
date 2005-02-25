70a8869acddbf62b5c3aecef22a41a4dd35548bf
Miek Gieben <miekg@NLnetLabs.nl>
2005-02-25 15:19:04 +0000

    mucho dns function made generic key conversion routines everything compiles, but remains untested
    crypto details (+1/-1, etc). NOT CHECKED

--- ldns/dnssec.h
+++ ldns/dnssec.h
@@ -11,6 +11,8 @@
 #ifndef _DNSSEC_H_
 #define _DNSSEC_H_
 
+#include <openssl/ssl.h>
+
 #define LDNS_RSAMD5		1
 #define LDNS_DH			2
 #define LDNS_DSA		3
@@ -19,5 +21,17 @@
 #define LDNS_INDIRECT		252
 #define LDNS_PRIVATEDNS		253
 #define LDNS_PRIVATEOID		254
+
+
+/* prototypes */
+
+bool ldns_verify_rrsig_dsa(ldns_buffer *, ldns_buffer *, ldns_buffer *);
+bool ldns_verify_rrsig_rsasha1(ldns_buffer *, ldns_buffer *, ldns_buffer *);
+bool ldns_verify_rrsig_rsamd5(ldns_buffer *, ldns_buffer *, ldns_buffer *);
+bool ldns_verify_rrsig(ldns_rr_list *, ldns_rr *, ldns_rr_list *);
+uint16_t ldns_keytag(ldns_rr *);
+DSA *ldns_key_buf2dsa(ldns_buffer *);
+RSA *ldns_key_buf2rsa(ldns_buffer *);
+	
 
 #endif /* _DNSSEC_H_ */
