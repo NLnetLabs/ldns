2100dc03d91701c2d3ae52a517f970a82dabfa14
Miek Gieben <miekg@NLnetLabs.nl>
2005-03-03 10:38:43 +0000

    things added

---
+++ trunk/keys.c
@@ -0,0 +1,7 @@
+/*
+ * keys.c handle private keys for use in DNSSEC
+ *
+ * This module should hide some of the openSSL complexities
+ * and give a general interface for private keys and hmac
+ * handling
+ */
