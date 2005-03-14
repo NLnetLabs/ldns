1480617a2af59fc930857402a90eff8e19a7b517
Jelte Jansen <jeltejan@NLnetLabs.nl>
2005-03-14 10:02:19 +0000

    function got missed in rev 490

--- rdata.c
+++ rdata.c
@@ -277,8 +277,8 @@ ldns_rdf *
 ldns_rdf_deep_clone(const ldns_rdf *r)
 {
 	return (ldns_rdf_new_frm_data(
-				ldns_rdf_size(r), 
 				ldns_rdf_get_type(r),
+				ldns_rdf_size(r), 
 				ldns_rdf_data(r)));
 }
 
