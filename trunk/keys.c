11cfbc77cf59f73ae64bbddd4878dae8cfa20cda
Miek Gieben <miekg@NLnetLabs.nl>
2005-03-16 13:21:12 +0000

    key_list added waiting for the BIG RENAME OP

--- trunk/keys.c
+++ trunk/keys.c
@@ -8,4 +8,71 @@
 
 #include <config.h>
 
+#include <ldns/keys.h>
+
 #include <util.h>
+
+ldns_key_list *
+ldns_key_list_new()
+{
+	ldns_key_list *key_list = MALLOC(ldns_key_list);
+	key_list->_key_count = 0;
+	key_list->_keys = NULL;
+	return key_list;
+}
+
+
+/* read */
+size_t
+ldns_key_list_key_count(ldns_key_list *key_list)
+{
+	        return key_list->_key_count;
+}       
+
+ldns_key *
+ldns_key_list_key(ldns_key_list *key, size_t nr)
+{       
+	if (nr < ldns_key_list_key_count(key)) {
+		return key->_keys[nr];
+	} else {
+		return NULL;
+	}
+}
+
+/* write */
+void            
+ldns_key_list_set_key_count(ldns_key_list *key, size_t count)
+{
+	        key->_key_count = count;
+}       
+
+/**     
+ * push an key to a keylist
+ * \param[in] key_list the key_list to push to 
+ * \param[in] key the key to push 
+ * \return false on error, otherwise true
+ */             
+bool             
+ldns_key_list_push_key(ldns_key_list *key_list, ldns_key *key)
+{       
+        size_t key_count;
+        ldns_key **keys;
+
+        key_count = ldns_key_list_key_count(key_list);
+        
+        /* grow the array */
+        keys = XREALLOC(
+                key_list->_keys, ldns_key *, key_count + 1);
+        if (!keys) {
+                return false;
+        }
+
+        /* add the new member */
+        key_list->_keys = keys;
+        key_list->_keys[key_count] = key;
+
+        ldns_key_list_set_key_count(key_list, key_count + 1);
+        return true;
+}
+
+
