e29577f55a3be83d99c7b2d72b9fd78c099a44b3
Miek Gieben <miekg@NLnetLabs.nl>
2005-04-13 11:05:44 +0000

    really clean the resolver list

--- trunk/resolver.c
+++ trunk/resolver.c
@@ -748,7 +748,6 @@ ldns_resolver_send(ldns_resolver *r, ldns_rdf *name, ldns_rr_type type, ldns_rr_
 	/* TODO: make last 3 arguments optional too? maybe make complete
 	         rr instead of seperate values in resolver (and packet)
 	*/
-dprintf("RESALGO: %s\n", ldns_resolver_tsig_algorithm(r));
 	if (ldns_resolver_tsig_keyname(r) && ldns_resolver_tsig_keydata(r)) {
 		status = ldns_pkt_tsig_sign(query_pkt,
 		                            ldns_resolver_tsig_keyname(r),
