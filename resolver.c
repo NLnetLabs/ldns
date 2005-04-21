ab113cb8e910d20420a433f01584e0ae15215096
Miek Gieben <miekg@NLnetLabs.nl>
2005-04-21 13:12:12 +0000

    obey _retrans and sleep the amount of time. Default value set to 2

--- resolver.c
+++ resolver.c
@@ -417,7 +417,7 @@ ldns_resolver_new(void)
 	ldns_resolver_set_domain(r, NULL);
 	ldns_resolver_set_defnames(r, false);
 	ldns_resolver_set_retry(r, 4);
-	ldns_resolver_set_retrans(r, 5);
+	ldns_resolver_set_retrans(r, 2);
 	ldns_resolver_set_fail(r, false);
 	ldns_resolver_set_edns_udp_size(r, 0);
 
