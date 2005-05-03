03322bb40ff4cc246bed0a5fcfd7777e50492216
Miek Gieben <miekg@NLnetLabs.nl>
2005-05-03 10:50:37 +0000

    free mem

--- higher.c
+++ higher.c
@@ -197,15 +197,14 @@ ldns_getaddrinfo(ldns_resolver *res, ldns_rdf *node, ldns_rr_class c, ldns_rr_li
 
 	t = ldns_rdf_get_type(node);
 	names_found = 0;
+	r = res;
 
 	if (res == NULL) {
 		/* prepare a new resolver, using /etc/resolv.conf is a guide  */
 		r = ldns_resolver_new_frm_file(NULL);
 		if (!r) {
 			return 0;
-		} else {
-		 	r = res;
-		}
+		} 
 	}
 
 	if (t == LDNS_RDF_TYPE_DNAME) {
@@ -220,6 +219,10 @@ ldns_getaddrinfo(ldns_resolver *res, ldns_rdf *node, ldns_rr_class c, ldns_rr_li
 		*ret = ldns_get_rr_list_name_by_addr(
 				r, node, c, 0);
 		names_found = ldns_rr_list_rr_count(*ret);
+	}
+
+	if (res == NULL) {
+		ldns_resolver_free(r);
 	}
 	
 	return names_found;
