d8b38d71e326e7399e5f92d70206de4d022b485c
Miek Gieben <miekg@NLnetLabs.nl>
2005-05-23 13:14:45 +0000

    manpage synopsis changes

--- trunk/doc/doxyparse.pl
+++ trunk/doc/doxyparse.pl
@@ -196,6 +196,18 @@ foreach (keys %manpages) {
 	print MAN  join ", ", @$name;
 	print MAN  "\n\n";
 	print MAN  ".SH SYNOPSIS\n";
+
+	print MAN  "#ifdef HAVE_STDINT_H\n.br\n";
+	print MAN  "#include <stdint.h>\n.br\n";
+	print MAN  "#endif /* HAVE_STDINT_H */\n";
+
+	print MAN  ".PP\n";
+
+	print MAN  "#ifdef HAVE_STDBOOL_H\n.br\n";
+	print MAN  "#include <stdbool.h>\n.br\n";
+	print MAN  "#endif /* HAVE_STDBOOL_H */\n";
+
+	print MAN  ".PP\n";
 	print MAN  "#include <ldns/ldns.h>\n";
 	print MAN  ".PP\n";
 
