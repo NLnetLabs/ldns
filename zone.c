f7ce4e1a365b65a244f35c9a60c41e066bb30629
Miek Gieben <miekg@NLnetLabs.nl>
2005-06-20 12:00:23 +0000

    skel function added. *pfew* it's to hot

--- zone.c
+++ zone.c
@@ -52,3 +52,17 @@ ldns_zone_set_rrs(ldns_zone *z, ldns_rr_list *rrlist)
 {
 	z->_rrs = rrlist;
 }
+
+/**
+ * ixfr function. Work on a ldns_zone and remove and add
+ * the rrs from the rrlist
+ * \param[in] z the zone to work on
+ * \param[in] del rr_list to remove from the zone
+ * \param[in] add rr_list to add to the zone
+ * \return Tja, wat zouden we eens returnen TODO
+ */
+void
+ldns_zone_ixfr_del_add(ldns_zone *z, ldns_rr_list *del, ldns_rr_list *add)
+{
+	
+}
