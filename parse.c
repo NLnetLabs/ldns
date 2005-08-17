07c16985783823e73e8108f9fe577d3c7feced20
Jelte Jansen <jeltejan@NLnetLabs.nl>
2005-08-17 09:48:58 +0000

    make line parser continue not return empty strings or strings that only contain '\n'

--- parse.c
+++ parse.c
@@ -151,13 +151,14 @@ ldns_fget_token_l(FILE *f, char *token, const char *delim, size_t limit, int *li
 
 		/* check if we hit the delim */
 		for (d = del; *d; d++) {
-                        if (c == *d) {
+                        if (c == *d && i > 1) {
 				goto tokenread;
                         }
 		}
-
-		*t++ = c;
-		i++;
+		if (c != '\0' && c != '\n') {
+			*t++ = c;
+			i++;
+		}
 		if (limit > 0 && i >= limit) {
 			*t = '\0';
 			return -1;
