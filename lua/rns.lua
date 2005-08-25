d777bbc22831bce7a773f1c5f714e1f2cca988db
Miek Gieben <miekg@NLnetLabs.nl>
2005-08-25 09:54:18 +0000

    fmt/layout changes

--- lua/rns.lua
+++ lua/rns.lua
@@ -14,49 +14,46 @@ pkt = packet.push_rr(pkt, LDNS_SECTION_ANSWER, rr4)
 pkt = packet.push_rr(pkt, LDNS_SECTION_AUTHORITY, rr2)
 pkt = packet.push_rr(pkt, LDNS_SECTION_AUTHORITY, rr3)
 
----- Setup a server to listen to UDP
+---- Setup a server to listen to UDP -- bit strange to first
+-- make a rdf out of it and then continue with the sockaddr struct
 rdf_ip = rdf.new_frm_str(LDNS_RDF_TYPE_A, "127.0.0.1")
 socket = udp.open(rdf_ip, 5353)
 
-
 while true do
+	-- read from the socket, this blocks...
+	wirebuf, sockaddr_from  = udp.read(socket)
 
--- read from the socket, this blocks...
-wirebuf, sockaddr_from  = udp.read(socket)
+	-- wrap this in new functions
+	if wirebuf == nil then
+		lua_debug("nothing received")
+	else
+		-- somebody is listening
+		wirepkt = buffer.to_pkt(wirebuf)
 
--- wrap this in new functions
-if wirebuf == nil then
-	lua_debug("nothing received")
-else
-	-- somebody is listening
-	wirepkt = buffer.to_pkt(wirebuf)
+		lua_debug("received from the interface")
 
-	lua_debug("received from the interface")
+		-- next we must send it to our recursive nameserver
+		-- and pick up the result
+		-- then we modify the result somewhat and sent it back
+		-- to the client
 
-	-- next we must send it to our recursive nameserver
-	-- and pick up the result
-	-- then we modify the result somewhat and sent it back
-	-- to the client
+		id = packet.id(wirepkt);
+		packet.print(wirepkt)
 
-	id = packet.id(wirepkt);
-	packet.print(wirepkt)
+		-- set the id on the outgoing packet
+		packet.set_id(pkt, id)
+		lua_packet_ancount_incr(pkt, 2)
+		wirebuf2 = packet.to_buf(pkt)
 
-	-- set the id on the outgoing packet
-	packet.set_id(pkt, id)
-	lua_packet_ancount_incr(pkt, 2)
-	wirebuf2 = packet.to_buf(pkt)
-
-	-- write back to the client
-	bytes = lua_udp_write(socket, wirebuf2, sockaddr_from)
-	if bytes == -1 then
-		lua_debug("write error")
-	else 
-		lua_debug("wrote bytes", bytes)
-		packet.print(pkt)
+		-- write back to the client
+		bytes = lua_udp_write(socket, wirebuf2, sockaddr_from)
+		if bytes == -1 then
+			lua_debug("write error")
+		else 
+			lua_debug("wrote bytes", bytes)
+			packet.print(pkt)
+		end
+		
 	end
-	
 end
-
-end
-
 udp.close(socket)
