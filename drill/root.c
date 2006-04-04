e28de7841220938d182b3119cf338ed831cc2074
Miek Gieben <miekg@NLnetLabs.nl>
2006-04-04 12:30:06 +0000

    fixes: lint and 12 unit test

--- drill/root.c
+++ drill/root.c
@@ -23,30 +23,30 @@ init_root(void)
 	
 	global_dns_root = ldns_rr_list_new();
 
-	ldns_rr_new_frm_str(&r, "a.root-servers.net 3600 IN A 198.41.0.4", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "a.root-servers.net 3600 IN A 198.41.0.4", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "b.root-servers.net 3600 IN A 192.228.79.201", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "b.root-servers.net 3600 IN A 192.228.79.201", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "c.root-servers.net 3600 IN A 192.33.4.12", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "c.root-servers.net 3600 IN A 192.33.4.12", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "d.root-servers.net 3600 IN A 128.8.10.90", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "d.root-servers.net 3600 IN A 128.8.10.90", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "e.root-servers.net 3600 IN A 192.203.230.10", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "e.root-servers.net 3600 IN A 192.203.230.10", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "f.root-servers.net 3600 IN A 192.5.5.241", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "f.root-servers.net 3600 IN A 192.5.5.241", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "g.root-servers.net 3600 IN A 192.112.36.4", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "g.root-servers.net 3600 IN A 192.112.36.4", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "h.root-servers.net 3600 IN A 128.63.2.53", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "h.root-servers.net 3600 IN A 128.63.2.53", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "i.root-servers.net 3600 IN A 192.36.148.17", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "i.root-servers.net 3600 IN A 192.36.148.17", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "j.root-servers.net 3600 IN A 192.58.128.30", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "j.root-servers.net 3600 IN A 192.58.128.30", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "k.root-servers.net 3600 IN A 193.0.14.129", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "k.root-servers.net 3600 IN A 193.0.14.129", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "l.root-servers.net 3600 IN A 198.32.64.12", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "l.root-servers.net 3600 IN A 198.32.64.12", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
-	ldns_rr_new_frm_str(&r, "m.root-servers.net 3600 IN A 202.12.27.33", 0, NULL, NULL);
+	(void)ldns_rr_new_frm_str(&r, "m.root-servers.net 3600 IN A 202.12.27.33", 0, NULL, NULL);
 	ldns_rr_list_push_rr(global_dns_root, r);
 }
