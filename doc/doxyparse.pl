d1bbc58409b8cd0b1d681faf67bd387c10bc503e
Miek Gieben <miekg@NLnetLabs.nl>
2006-04-13 14:06:29 +0000

    up2date

--- doc/doxyparse.pl
+++ doc/doxyparse.pl
@@ -37,8 +37,8 @@ my $BASE="doc/man";
 my $MAN_SECTION = "3";
 my $MAN_HEADER = ".TH ldns  \"25 Apr 2005\"\n";
 my $MAN_MIDDLE = ".SH AUTHOR
-The ldns team at NLnet Labs. Which consists out of: 
-Jelte Jansen, Erik Rozendaal and Miek Gieben.
+The ldns team at NLnet Labs. Which consists out of
+Jelte Jansen and Miek Gieben.
 
 .SH REPORTING BUGS
 Please report bugs to ldns-team\@nlnetlabs.nl or in 
@@ -48,7 +48,7 @@ http://www.nlnetlabs.nl/bugs/index.html
 Be sure to select ldns as the product.
 
 .SH COPYRIGHT
-Copyright (c) 2004, 2005 NLnet Labs.
+Copyright (c) 2004 - 2006 NLnet Labs.
 Licensed under the BSD License. There is NO warranty; not even for
 MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE.
