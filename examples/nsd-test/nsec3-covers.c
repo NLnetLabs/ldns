990452c5b753041874cbf1fcb41632eca05c0f79
Wouter Wijngaards <wouter@NLnetLabs.nl>
2006-04-25 09:55:55 +0000

    pipe dig output and get output such as: nsec3-covers: read 8 rrs nsec3-covers: qname is bla. tnr68pkfhhgpkcn2jg3oe0fpb3h63l6p. proves bla. does not exist. gqmpmj04c1k2m91euke181pok7rug3eh. proves . exists. ps0vors542hsre5t6tiu5jtk7kajh6hl. proves *. does not exist.

--- examples/nsd-test/nsec3-covers.c
+++ examples/nsd-test/nsec3-covers.c
@@ -23,18 +23,12 @@ void abort_ldns_error(const char* str, ldns_status err)
 	exit(1);
 }
 
-int
+char*
 skip_comments_and_query(FILE* in, ldns_rdf ** qname)
 {
-	char buf[10240];
-	fpos_t start;
+	static char buf[10240];
 	/* read comment lines */
 	while(1) {
-		/* remember start of this line in case it is an RR */
-		if(fgetpos(in, &start) == -1) {
-			fprintf(stderr, "could not fgetpos: %s", strerror(errno));
-			exit(1);
-		}
 		if(!fgets(buf, sizeof(buf), in))
 			return 0; /* EOF */
 		printf("%s", buf); /* echo */
@@ -54,23 +48,21 @@ skip_comments_and_query(FILE* in, ldns_rdf ** qname)
 			*qname = ldns_dname_new_frm_str(q_rr);
 		}
 	}
-	if(fsetpos(in, &start) == -1) {
-		fprintf(stderr, "could not fsetpos: %s", strerror(errno));
-		exit(1);
-	}
-	return 1;
+	return buf;
 }
 
 void
 read_in(ldns_rr_list* list, ldns_rdf** qname, FILE *in)
 {
-	while(skip_comments_and_query(in, qname))
+	char* buf;
+	while((buf=skip_comments_and_query(in, qname)))
 	{
 		/* add rr */
 		ldns_rr *rr=0;
 		ldns_rdf *origin=0, *prev=0;
 		ldns_status err;
-		if((err=ldns_rr_new_frm_fp(&rr, in, NULL, &origin, &prev)) != 
+		uint16_t ttl = 3600;
+		if((err=ldns_rr_new_frm_str(&rr, buf, ttl, origin, &prev)) != 
 			LDNS_STATUS_OK)
 			abort_ldns_error("read rr", err);
 		ldns_rr_list_push_rr(list, rr);
@@ -85,10 +77,37 @@ read_in(ldns_rr_list* list, ldns_rdf** qname, FILE *in)
 	printf("\n");
 }
 
+struct donelist {
+	ldns_rdf* name;
+	struct donelist* next;
+};
+static struct donelist *done = 0;
+
+/* this is a linear speed test (slow for large numbers).
+   but the dig response will be small anyway. */
+int check_done(ldns_rdf *qname)
+{
+	struct donelist* p = done;
+	while(p) {
+		if(ldns_dname_compare(qname, p->name)==0)
+			return 1;
+		p = p->next;
+	}
+	/* not done yet add to list */
+	p = (struct donelist*)malloc(sizeof(struct donelist));
+	p->name = qname;
+	p->next = done;
+	done = p;
+	return 0;
+}
+
 void
 check_cover(ldns_rr_list *list, ldns_rdf *qname)
 {
+	ldns_status status;
 	size_t i;
+	if(check_done(qname))
+		return;
 	for(i=0; i<ldns_rr_list_rr_count(list); ++i)
 	{
 		ldns_rr* nsec3 = ldns_rr_list_rr(list, i);
@@ -98,24 +117,22 @@ check_cover(ldns_rr_list *list, ldns_rdf *qname)
 		}
 		ldns_rdf* hashed = ldns_nsec3_hash_name_frm_nsec3(
 			nsec3, qname);
-		printf("qname ");
-		ldns_rdf_print(stdout, qname);
-		printf(" with nsec3 ");
-		ldns_rr_print(stdout, nsec3);
-		printf(" hashes to ");
-		ldns_rdf_print(stdout, hashed);
-		printf("\n");
+		status = ldns_dname_cat(hashed, ldns_dname_left_chop(
+			ldns_rr_owner(nsec3)));
+		if(status != LDNS_STATUS_OK)
+			abort_ldns_error("ldns_dname_cat", status);
+
 		if(ldns_dname_compare(hashed, ldns_rr_owner(nsec3)) == 0) {
-			ldns_rdf_print(stdout, qname);
-			printf(" is validated by ");
 			ldns_rdf_print(stdout, ldns_rr_owner(nsec3));
-			printf("\n");
+			printf(" proves ");
+			ldns_rdf_print(stdout, qname);
+			printf(" exists.\n");
 		}
 		else if(ldns_nsec_covers_name(nsec3, hashed)) {
-			ldns_rdf_print(stdout, qname);
-			printf(" is covered by ");
 			ldns_rdf_print(stdout, ldns_rr_owner(nsec3));
-			printf("\n");
+			printf(" proves ");
+			ldns_rdf_print(stdout, qname);
+			printf(" does not exist.\n");
 		}
 		ldns_rdf_free(hashed);
 	}
@@ -124,12 +141,22 @@ check_cover(ldns_rr_list *list, ldns_rdf *qname)
 void
 covertests(ldns_rr_list *list, ldns_rdf *qname)
 {
-	check_cover(list, qname);
+	size_t i;
+	ldns_rdf *smaller = qname;
+	ldns_rdf *wcard = ldns_dname_new_frm_str("*");
+	for(i=0; i<ldns_dname_label_count(qname)+1; ++i)
+	{
+		check_cover(list, smaller);
+		ldns_rdf* wcardchild = ldns_dname_cat_clone(wcard, smaller);
+		check_cover(list, wcardchild);
+		smaller = ldns_dname_left_chop(smaller);
+	}
 }
 
 int
 main(int argc, char **argv)
 {
+	size_t i;
 	if(argc != 1) {
 		usage(stderr);
 		return 0;
@@ -142,6 +169,13 @@ main(int argc, char **argv)
 
 	/* check covers */
 	covertests(list, qname);
+	for(i=0; i<ldns_rr_list_rr_count(list); ++i)
+	{
+		ldns_rr* rr = ldns_rr_list_rr(list, i);
+		if(!ldns_dname_is_subdomain(qname, ldns_rr_owner(rr))) {
+			covertests(list, ldns_rr_owner(rr));
+		}
+	}
 
 	ldns_rr_list_deep_free(list);
 	return 0;
