669a6e55b01ccebda1cf2d32d0419e05cbdf4f18
Jelte Jansen <jeltejan@NLnetLabs.nl>
2006-06-30 09:05:16 +0000

    let resolver_frm_fp_l report correct line nr let ldns-resolver return error code on error test 17 should fail now

--- examples/ldns-resolver.c
+++ examples/ldns-resolver.c
@@ -7,6 +7,7 @@
  */
 
 #include "config.h"
+#include "errno.h"
 
 #include <ldns/dns.h>
 
@@ -14,23 +15,26 @@ int
 main(int argc, char **argv) {
 
 	ldns_resolver *r;
-	int line;
+	int line = 1;
 	FILE *rand;
 	ldns_status s;
 
-	if (argc < 2) {
+	if (argc != 2 || strncmp(argv[1], "-h", 3) == 0) {
 		printf("Usage: ldns-resolver <file>\n");
+		printf("Tries to create a stub resolver structure from the given file.\n");
 		exit(EXIT_FAILURE);
 	}
 	
 	if (!(rand = fopen(argv[1], "r"))) {
+		printf("Error opening %s: %s\n", argv[1], strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 
 	printf("Trying to read from %s\n", argv[1]);
 	s = ldns_resolver_new_frm_fp_l(&r, rand, &line);
 	if (s != LDNS_STATUS_OK) {
-		printf("Failure\n");
+		printf("Failed: %s at line %d\n", ldns_get_errorstr_by_id(s), line);
+		exit(EXIT_FAILURE);
 	} else {
 		printf("Succes\n");
 		ldns_resolver_print(stdout, r);
