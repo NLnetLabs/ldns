6522dafb1ba5e1bc67b48de6b1355324baa9bd5f
Wouter Wijngaards <wouter@NLnetLabs.nl>
2007-04-23 14:58:21 +0000

    Slightly more lenient: if an EDNS OPT record is not the last in the additional section, this is accepted by wireformat to ldnspkt routine. It no longer drops the last RR from the additional section in that case.

--- wire2host.c
+++ wire2host.c
@@ -351,6 +351,7 @@ ldns_wire2pkt(ldns_pkt **packet_p, const uint8_t *wire, size_t max)
 	ldns_rr *rr;
 	ldns_pkt *packet = ldns_pkt_new();
 	ldns_status status = LDNS_STATUS_OK;
+	int have_edns = 0;
 
 	uint8_t data[4];
 
@@ -409,7 +410,7 @@ ldns_wire2pkt(ldns_pkt **packet_p, const uint8_t *wire, size_t max)
 				ldns_pkt_set_edns_data(packet, ldns_rdf_clone(ldns_rr_rdf(rr, 0)));
 			}
 			ldns_rr_free(rr);
-			ldns_pkt_set_arcount(packet, ldns_pkt_arcount(packet) - 1);
+			have_edns = 1;
 		} else if (ldns_rr_get_type(rr) == LDNS_RR_TYPE_TSIG) {
 			ldns_pkt_set_tsig(packet, rr);
 			ldns_pkt_set_arcount(packet, ldns_pkt_arcount(packet) - 1);
@@ -419,6 +420,8 @@ ldns_wire2pkt(ldns_pkt **packet_p, const uint8_t *wire, size_t max)
 		}
 	}
 	ldns_pkt_set_size(packet, max);
+	if(have_edns)
+		ldns_pkt_set_arcount(packet, ldns_pkt_arcount(packet) - 1);
 
 	*packet_p = packet;
 	return status;
