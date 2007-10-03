e22d25c34082d04378326f61e876c464837bc41d
Jelte Jansen <jeltejan@NLnetLabs.nl>
2007-10-03 09:58:01 +0000

    backport of rdata length skip in rr comparison

--- rr.c
+++ rr.c
@@ -1337,7 +1337,9 @@ int ldns_rr_compare_wire(ldns_buffer *rr1_buf, ldns_buffer *rr2_buf)
         while (offset < rr1_len && *ldns_buffer_at(rr1_buf, offset) != 0) {
           offset += *ldns_buffer_at(rr1_buf, offset) + 1;
         }
-        offset += 9;
+        /* jump to rdata section (PAST the rdata length field, otherwise
+           rrs with different lengths might be sorted erroneously */
+        offset += 11;
 	min_len = (rr1_len < rr2_len) ? rr1_len : rr2_len;
         /* Compare RRs RDATA byte for byte. */
         for(i = offset; i < min_len; i++) {
