6b31444559131588af18d98bfd8963c27a3da075
Wouter Wijngaards <wouter@NLnetLabs.nl>
2007-10-18 10:10:17 +0000

    remove autom4te caches from drill and examples for release tarballs.

--- makedist.sh
+++ makedist.sh
@@ -131,7 +131,7 @@ cd examples && autoreconf && cd .. || error_cleanup "Autoconf failed."
 info "Building configure script for drill (autoconf)."
 cd drill && autoreconf && cd .. || error_cleanup "Autoconf failed."
 
-rm -r autom4te* || error_cleanup "Failed to remove autoconf cache directory."
+rm -r autom4te* drill/autom4te* examples/autom4te* || error_cleanup "Failed to remove autoconf cache directory."
 
 # custom removes
 find . -name .c-mode-rc.el -exec rm {} \;
