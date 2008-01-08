60aba3d863a24d3d788ca89252f4483118cd5b4f
Wouter Wijngaards <wouter@NLnetLabs.nl>
2008-01-08 13:43:11 +0000

    return value from sscanf checked correctly.

--- str2host.c
+++ str2host.c
@@ -61,7 +61,7 @@ ldns_str2rdf_time(ldns_rdf **rd, const char *time)
 	memset(&tm, 0, sizeof(tm));
 
 	if (strlen(time) == 14 &&
-	    sscanf(time, "%4d%2d%2d%2d%2d%2d", &tm.tm_year, &tm.tm_mon, &tm.tm_mday, &tm.tm_hour, &tm.tm_min, &tm.tm_sec)
+	    sscanf(time, "%4d%2d%2d%2d%2d%2d", &tm.tm_year, &tm.tm_mon, &tm.tm_mday, &tm.tm_hour, &tm.tm_min, &tm.tm_sec) == 6
 	   ) {
 	   	tm.tm_year -= 1900;
 	   	tm.tm_mon--;
