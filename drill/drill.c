932afd8ee4052da26c4724cd19115a234b8de545
Matthijs Mekking <matje@NLnetLabs.nl>
2009-11-26 09:33:47 +0000

    better drill warning

--- drill/drill.c
+++ drill/drill.c
@@ -467,8 +467,10 @@ main(int argc, char *argv[])
 		/* no server given make a resolver from /etc/resolv.conf */
 		status = ldns_resolver_new_frm_file(&res, resolv_conf_file);
 		if (status != LDNS_STATUS_OK) {
-			warning("Could not create a resolver structure: %s",
-				   ldns_get_errorstr_by_id(status));
+			warning("Could not create a resolver structure, %s\n"
+					"%s: %s\n"
+					"Try drill @localhost if you have a resolver running on your machine.",
+				    ldns_get_errorstr_by_id(status), strerror(errno), resolv_conf_file);
 			result = EXIT_FAILURE;
 			goto exit;
 		}
