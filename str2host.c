0d1f9553c7ff28947a738249b4304d199e86b332
Matthijs Mekking <matje@NLnetLabs.nl>
2012-01-11 15:59:22 +0000

    \DDD can only be [0...255]

--- str2host.c
+++ str2host.c
@@ -259,17 +259,21 @@ ldns_str2rdf_int8(ldns_rdf **rd, const char *bytestr)
  */
 static int
 parse_escape(uint8_t *s, uint8_t *q) {
-	uint8_t val;
+	uint16_t val;
 	if (strlen((char *)s) > 3 &&
 	    isdigit((int) s[1]) &&
 	    isdigit((int) s[2]) &&
 	    isdigit((int) s[3])) {
 		/* cast this so it fits */
-		val = (uint8_t) ldns_hexdigit_to_int((char) s[1]) * 100 +
+		val = (uint16_t) ldns_hexdigit_to_int((char) s[1]) * 100 +
 		                ldns_hexdigit_to_int((char) s[2]) * 10 +
 		                ldns_hexdigit_to_int((char) s[3]);
-		*q = val;
-		return 3;
+		if (val > 255) {
+			/* outside range */
+			return 0;
+		}
+		*q = (uint8_t) val;
+                return 3;
 	} else {
 		s++;
 		if (*s == '\0' || isdigit((int) *s)) {
