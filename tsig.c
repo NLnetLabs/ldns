8b36efd701e9c88a9401607a09260b56e3cb896e
Michael Weiser <michael.weiser@gmx.de>
2016-12-14 14:36:05 +0100

    Add sha384 and sha512 tsig algorithm

--- tsig.c
+++ tsig.c
@@ -134,7 +134,19 @@ ldns_digest_function(char *name)
 {
 	/* these are the mandatory algorithms from RFC4635 */
 	/* The optional algorithms are not yet implemented */
-	if (strcasecmp(name, "hmac-sha256.") == 0) {
+	if (strcasecmp(name, "hmac-sha512.") == 0) {
+#ifdef HAVE_EVP_SHA512
+		return EVP_sha512();
+#else
+		return NULL;
+#endif
+	} else if (strcasecmp(name, "hmac-shac384.") == 0) {
+#ifdef HAVE_EVP_SHA384
+		return EVP_sha384();
+#else
+		return NULL;
+#endif
+	} else if (strcasecmp(name, "hmac-sha256.") == 0) {
 #ifdef HAVE_EVP_SHA256
 		return EVP_sha256();
 #else
