9606d16dc2b5fd6d3feff8169b4c17c55e82ce42
Jeffrey Walton <noloader@gmail.com>
2020-02-24 06:34:15 -0500

    Add SHA-384 and SHA-512 to 13-unit-tests-base.c Whitespace check-in on 13-unit-tests-base.c

--- test/13-unit-tests-base.tpkg/13-unit-tests-base.c
+++ test/13-unit-tests-base.tpkg/13-unit-tests-base.c
@@ -11,26 +11,25 @@
 
 void print_data_ar(const uint8_t *data, const size_t len) {
 	size_t i;
-	
+
 	for (i = 0; i < len; i++) {
 		printf("%02x ", data[i]);
 	}
 }
 
-
 int
 test_base64_encode(uint8_t *data, size_t data_len, const char *expect_result)
 {
 	int result;
-	
+
 	char *text;
 	size_t text_len;
 
 	text_len = ldns_b64_ntop_calculate_size(data_len);
 	text = malloc(text_len);
-	
+
 	result = ldns_b64_ntop(data, data_len, text, text_len);
-	
+
 	text_len = result;
 
 	if (result < 0) {
@@ -47,7 +46,7 @@ test_base64_encode(uint8_t *data, size_t data_len, const char *expect_result)
 			printf("Data:\t");
 			print_data_ar(data, data_len);
 			printf("\n");
-			
+
 			result = 2;
 		} else {
 			result = 0;
@@ -61,20 +60,20 @@ int
 test_base64_decode(const char *str, const uint8_t *expect_data, size_t expect_data_len)
 {
 	int result;
-	
+
 	uint8_t *data;
 	size_t data_len;
 
 	size_t i;
 
 	data_len = ldns_b64_pton_calculate_size(strlen(str));
-	
+
 	data = malloc(data_len);
-	
+
 	result = ldns_b64_pton(str, data, data_len);
-	
+
 	data_len = result;
-	
+
 	if (result < 0) {
 		printf("Error 2 decoding base64 test data (return code %d): %s\n", result, str);
 		result = 1;
@@ -106,7 +105,7 @@ test_base64_decode(const char *str, const uint8_t *expect_data, size_t expect_da
 			}
 		}
 	}
-	
+
 	return result;
 }
 
@@ -114,15 +113,15 @@ int
 test_base32_encode(uint8_t *data, size_t data_len, const char *expect_result)
 {
 	int result;
-	
+
 	char *text;
 	size_t text_len;
 
 	text_len = ldns_b32_ntop_calculate_size(data_len) + 10;
 	text = malloc(text_len);
-	
+
 	result = ldns_b32_ntop(data, data_len, text, text_len);
-	
+
 	if (result < 0) {
 		printf("Error 3 encoding base32 test data (result %d):\n", result);
 		print_data_ar(data, data_len);
@@ -137,7 +136,7 @@ test_base32_encode(uint8_t *data, size_t data_len, const char *expect_result)
 			printf("Data:\t");
 			print_data_ar(data, data_len);
 			printf("\n");
-			
+
 			result = 2;
 		} else {
 			result = 0;
@@ -151,20 +150,20 @@ int
 test_base32_decode(const char *str, const uint8_t *expect_data, size_t expect_data_len)
 {
 	int result;
-	
+
 	uint8_t *data;
 	size_t data_len;
 
 	size_t i;
 
 	data_len = ldns_b32_pton_calculate_size(strlen(str))  +  10;
-	
+
 	data = malloc(data_len);
-	
+
 	result = ldns_b32_pton(str, strlen(str), data, data_len);
-	
+
 	data_len = result;
-	
+
 	if (result < 0) {
 		printf("Error 4 decoding base32 test data (result %d): %s\n", result, str);
 		result = 1;
@@ -196,7 +195,7 @@ test_base32_decode(const char *str, const uint8_t *expect_data, size_t expect_da
 			}
 		}
 	}
-	
+
 	return result;
 }
 
@@ -204,16 +203,16 @@ int
 test_base32_encode_extended_hex(uint8_t *data, size_t data_len, const char *expect_result)
 {
 	int result;
-	
+
 	char *text;
 	size_t text_len;
 
 	text_len = ldns_b32_ntop_calculate_size(data_len) + 10;
 	text = malloc(text_len);
-	
+
 	result = ldns_b32_ntop_extended_hex(data, data_len, text, text_len);
-	
-	
+
+
 	if (result < 0) {
 		printf("Error 5 encoding base32 extended hex test data (result %d):\n", result);
 		print_data_ar(data, data_len);
@@ -229,7 +228,7 @@ test_base32_encode_extended_hex(uint8_t *data, size_t data_len, const char *expe
 			printf("Data:\t");
 			print_data_ar(data, data_len);
 			printf("\n");
-			
+
 			result = 2;
 		} else {
 			result = 0;
@@ -243,20 +242,20 @@ int
 test_base32_decode_extended_hex(const char *str, const uint8_t *expect_data, size_t expect_data_len)
 {
 	int result;
-	
+
 	uint8_t *data;
 	size_t data_len;
 
 	size_t i;
 
 	data_len = ldns_b32_pton_calculate_size(strlen(str)) + 10;
-	
+
 	data = malloc(data_len);
-	
+
 	result = ldns_b32_pton_extended_hex(str, strlen(str), data, data_len);
-	
+
 	data_len = result;
-	
+
 	if (result < 0) {
 		printf("Error 6 decoding base32 extended hex test data (result %d): %s\n", result, str);
 		result = 1;
@@ -288,11 +287,9 @@ test_base32_decode_extended_hex(const char *str, const uint8_t *expect_data, siz
 			}
 		}
 	}
-	
+
 	return result;
 }
-
-
 
 int
 test_sha1(const void *data, const void *expect_result_str)
@@ -302,7 +299,7 @@ test_sha1(const void *data, const void *expect_result_str)
 	unsigned int digest_len;
 	uint8_t *expect_result;
 	size_t data_len;
-	
+
 	data_len = strlen(CH_PTR(data));
 
 	expect_result = malloc(strlen(CH_PTR(expect_result_str)) / 2);
@@ -310,7 +307,7 @@ test_sha1(const void *data, const void *expect_result_str)
 
 	digest_len = LDNS_SHA1_DIGEST_LENGTH;
 	digest = malloc(digest_len);
-	
+
 	d = ldns_sha1(data, data_len, digest);
 
 	if (!d) {
@@ -325,7 +322,7 @@ test_sha1(const void *data, const void *expect_result_str)
 			printf("Expected:                 ");
 			printf("%s\n", CH_PTR(expect_result));
 			printf("Data:\t%s\n", CH_PTR(data));
-			
+
 			result = 2;
 		} else {
 			result = 0;
@@ -344,7 +341,7 @@ test_sha256(const void *data, const void *expect_result_str)
 	unsigned int digest_len;
 	uint8_t *expect_result;
 	size_t data_len;
-	
+
 	data_len = strlen(CH_PTR(data));
 
 	expect_result = malloc(strlen(CH_PTR(expect_result_str)) / 2);
@@ -352,7 +349,7 @@ test_sha256(const void *data, const void *expect_result_str)
 
 	digest_len = LDNS_SHA256_DIGEST_LENGTH;
 	digest = malloc(digest_len);
-	
+
 	d = ldns_sha256(data, data_len, digest);
 
 	if (!d) {
@@ -367,7 +364,91 @@ test_sha256(const void *data, const void *expect_result_str)
 			printf("Expected:                 ");
 			printf("%s\n", CH_PTR(expect_result));
 			printf("Data:\t%s\n", CH_PTR(data));
-			
+
+			result = 2;
+		} else {
+			result = 0;
+		}
+	}
+	free(digest);
+	free(expect_result);
+	return result;
+}
+
+int
+test_sha384(const void *data, const void *expect_result_str)
+{
+	int result;
+	unsigned char *digest, *d;
+	unsigned int digest_len;
+	uint8_t *expect_result;
+	size_t data_len;
+
+	data_len = strlen(CH_PTR(data));
+
+	expect_result = malloc(strlen(CH_PTR(expect_result_str)) / 2);
+	(void) ldns_hexstring_to_data(expect_result, expect_result_str);
+
+	digest_len = LDNS_SHA384_DIGEST_LENGTH;
+	digest = malloc(digest_len);
+
+	d = ldns_sha384(data, data_len, digest);
+
+	if (!d) {
+		printf("Error in digest of test data (digesting failed):\n");
+		print_data_ar(data, data_len);
+		printf("\n");
+		result = 1;
+	} else {
+		if (strncmp(CH_PTR(expect_result), CH_PTR(digest), digest_len) != 0) {
+			printf("Bad sha384 digest: got: ");
+			print_data_ar(digest, digest_len);
+			printf("Expected:                 ");
+			printf("%s\n", CH_PTR(expect_result));
+			printf("Data:\t%s\n", CH_PTR(data));
+
+			result = 2;
+		} else {
+			result = 0;
+		}
+	}
+	free(digest);
+	free(expect_result);
+	return result;
+}
+
+int
+test_sha512(const void *data, const void *expect_result_str)
+{
+	int result;
+	unsigned char *digest, *d;
+	unsigned int digest_len;
+	uint8_t *expect_result;
+	size_t data_len;
+
+	data_len = strlen(CH_PTR(data));
+
+	expect_result = malloc(strlen(CH_PTR(expect_result_str)) / 2);
+	(void) ldns_hexstring_to_data(expect_result, expect_result_str);
+
+	digest_len = LDNS_SHA512_DIGEST_LENGTH;
+	digest = malloc(digest_len);
+
+	d = ldns_sha512(data, data_len, digest);
+
+	if (!d) {
+		printf("Error in digest of test data (digesting failed):\n");
+		print_data_ar(data, data_len);
+		printf("\n");
+		result = 1;
+	} else {
+		if (strncmp(CH_PTR(expect_result), CH_PTR(digest), digest_len) != 0) {
+			printf("Bad sha512 digest: got: ");
+			print_data_ar(digest, digest_len);
+			printf("Expected:                 ");
+			printf("%s\n", CH_PTR(expect_result));
+			printf("Data:\t%s\n", CH_PTR(data));
+
 			result = 2;
 		} else {
 			result = 0;
@@ -591,7 +672,7 @@ main(void)
 		result = EXIT_FAILURE;
 	}
 	free(data);
-	
+
 
 	/* BASE64("fo") = "Zm8=" */
 	/* BASE32("fo") = "MZXQ====" */
@@ -803,10 +884,11 @@ main(void)
 		result = EXIT_FAILURE;
 	}
 
-	if (test_sha1("abc", "A9993E364706816ABA3E25717850C26C9CD0D89D") != 0) {
+	/* SHA-1 */
+	if (test_sha1("", "da39a3ee5e6b4b0d3255bfef95601890afd80709") != 0) {
 		result = EXIT_FAILURE;
 	}
-	if (test_sha1("", "da39a3ee5e6b4b0d3255bfef95601890afd80709") != 0) {
+	if (test_sha1("abc", "A9993E364706816ABA3E25717850C26C9CD0D89D") != 0) {
 		result = EXIT_FAILURE;
 	}
 	if (test_sha1("Test vector from febooti.com", "a7631795f6d59cd6d14ebd0058a6394a4b93d868") != 0) {
@@ -816,10 +898,34 @@ main(void)
 		result = EXIT_FAILURE;
 	}
 
+	/* SHA-256 */
 	if (test_sha256("", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855") != 0) {
 		result = EXIT_FAILURE;
 	}
 	if (test_sha256("Test vector from febooti.com", "077b18fe29036ada4890bdec192186e10678597a67880290521df70df4bac9ab") != 0) {
+		result = EXIT_FAILURE;
+	}
+
+	/* SHA-384 */
+	if (test_sha384("", "38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b") != 0) {
+		result = EXIT_FAILURE;
+	}
+	if (test_sha384("Test vector from febooti.com", "388bb2d487de48740f45fcb44152b0b665428c49def1aaf7c7f09a40c10aff1cd7c3fe3325193c4dd35d4eaa032f49b0") != 0) {
+		result = EXIT_FAILURE;
+	}
+	if (test_sha384("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "edb12730a366098b3b2beac75a3bef1b0969b15c48e2163c23d96994f8d1bef760c7e27f3c464d3829f56c0d53808b0b") != 0) {
+		result = EXIT_FAILURE;
+	}
+
+	/* SHA-512 */
+	if (test_sha512("", "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e") != 0) {
+		result = EXIT_FAILURE;
+	}
+	if (test_sha512("Test vector from febooti.com", "09fb898bc97319a243a63f6971747f8e102481fb8d5346c55cb44855adc2e0e98f304e552b0db1d4eeba8a5c8779f6a3010f0e1a2beb5b9547a13b6edca11e8a") != 0) {
+		result = EXIT_FAILURE;
+	}
+
+	if (test_sha512("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "b73d1929aa615934e61a871596b3f3b33359f42b8175602e89f7e06e5f658a243667807ed300314b95cacdd579f3e33abdfbe351909519a846d465c59582f321") != 0) {
 		result = EXIT_FAILURE;
 	}
 
