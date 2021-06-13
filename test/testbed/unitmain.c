242b0401a1027522618350bfc9a00fc16c1b5902
Josh Soref <jsoref@users.noreply.github.com>
2021-06-13 17:42:22 -0400

    spelling: command line
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/testbed/unitmain.c
+++ test/testbed/unitmain.c
@@ -17,7 +17,7 @@
 /**
  * Main unit test program. Setup, teardown and report errors.
  * @param argc: arg count.
- * @param argv: array of commandline arguments.
+ * @param argv: array of command line arguments.
  */
 int main(int argc, char* argv[])
 {
