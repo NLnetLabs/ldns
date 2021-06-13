8f74b33d181a50e9fb8a64070d904994fb355ab8
Josh Soref <jsoref@users.noreply.github.com>
2021-06-13 17:31:58 -0400

    spelling: arithmetics
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- host2str.c
+++ host2str.c
@@ -418,7 +418,7 @@ ldns_rdf2buffer_str_time(ldns_buffer *output, const ldns_rdf *rdf)
 	char date_buf[16];
 
 	memset(&tm, 0, sizeof(tm));
-	if (ldns_serial_arithmitics_gmtime_r(ldns_rdf2native_int32(rdf), time(NULL), &tm)
+	if (ldns_serial_arithmetics_gmtime_r(ldns_rdf2native_int32(rdf), time(NULL), &tm)
 	    && strftime(date_buf, 15, "%Y%m%d%H%M%S", &tm)) {
 		ldns_buffer_printf(output, "%s", date_buf);
 	}
