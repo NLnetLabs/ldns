43ca9a07fa974589f2a9e36ba6041165a34adbba
Josh Soref <jsoref@users.noreply.github.com>
2021-06-13 21:06:01 -0400

    spelling: parentheses
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- parse.c
+++ parse.c
@@ -32,7 +32,7 @@ ldns_fget_token_l_st(FILE *f, char **token, size_t *limit, bool fixed
                     , const char *delim, int *line_nr)
 {
 	int c, prev_c;
-	int p; /* 0 -> no parenthese seen, >0 nr of ( seen */
+	int p; /* 0 -> no parentheses seen, >0 nr of ( seen */
 	int com, quoted;
 	char *t, *old_token;
 	size_t i;
@@ -271,7 +271,7 @@ ssize_t
 ldns_bget_token(ldns_buffer *b, char *token, const char *delim, size_t limit)
 {
 	int c, lc;
-	int p; /* 0 -> no parenthese seen, >0 nr of ( seen */
+	int p; /* 0 -> no parentheses seen, >0 nr of ( seen */
 	int com, quoted;
 	char *t;
 	size_t i;
